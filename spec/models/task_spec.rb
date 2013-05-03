41fa3647e41a67d5d7a3eb6302f4f0b62b8c3554
Pamela Overman <pwnela@gmail.com>
2013-05-03 10:40:15 -0400

    DRYs up task_spec

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,34 +1,34 @@
 require 'spec_helper'
 
 describe Task do
+  let!(:task) { Task.new(description: "Test Task") }
+
   it 'should have a description' do
-    task = Task.new(description: "Test Task")
     task.description.should eql "Test Task"
   end
 
   it 'is not valid if it does not have a description' do
-    task = Task.new
-    task.should_not be_valid
+    bad_task = Task.new
+    bad_task.should_not be_valid
   end
 
   it 'is by default marked as incomplete' do
-    task = Task.new(description: "New Test Task")
     task.should_not be_completed
   end
 
   it 'can be marked complete' do
-    task = Task.new(description: "Test Task")
     task.mark_complete
     task.should be_completed
   end
 
   it 'tracks how far away its deadline is' do
-    task = Task.new(description: "Test Task", deadline: (Date.today + 1.days) )
+    task.deadline = (Date.today + 1.days)
     task.days_left_to_complete.should == 1
   end
 
   it 'tracks if a deadline is past due' do
-    task = Task.new(description: "Test Task", deadline: (Date.today - 1.days) )
+    task.deadline = (Date.today - 1.days)
     task.days_left_to_complete.should == -1
   end
+  
 end
